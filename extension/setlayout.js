c3486294fd9ed07d830586839187f6ef93732db9
William Kwon <dubkwon@gmail.com>
2013-02-12 19:45:57 -0800

    Initial Commit
    Version 1.0

---
+++ extension/setlayout.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$(".album-settings-link")[0].click();
+	$("#cboxOverlay").hide();
+	$("#colorbox").hide();
+	
+	var $ischecked = $("#userLayoutForm #horizontal").attr('checked');
+	if (!$ischecked) {
+		$("#userLayoutForm #horizontal").attr('checked', 'checked');
+		$(".button-medium.userSessionLayout").click();
+	}
+})
